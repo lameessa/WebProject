b4e6224fdf4111c8693a737cf7f91a591554b4d5
lameessa <444201177@student.ksu.edu.sa>
2025-01-30 18:00:33 +0300

    final edits

--- js/index.js
+++ js/index.js
@@ -6,10 +6,6 @@
 				window.location.href = "LoginPage.html";
 			});
 
-			// Redirect when "Book Appointment" button is clicked
-			document.getElementById("appointment-button").addEventListener("click", () => {
-				window.location.href = "Appointment.html"; 
-			});
 
 			// Redirect when "Login" button is clicked in the split-section
 			const loginButton = document.querySelector(".login-button");
