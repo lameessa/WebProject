da38df4801d91be5a43791ad7b7191b40f5fe38f
lameessa <444201177@student.ksu.edu.sa>
2025-04-05 20:52:01 +0300

    Update Rifq.sql

--- Rifq/Rifq.sql
+++ Rifq/Rifq.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Mar 17, 2025 at 10:56 PM
--- Server version: 8.0.40
--- PHP Version: 8.3.14
+-- Host: localhost:3306
+-- Generation Time: Apr 05, 2025 at 05:51 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,75 +18,75 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Rifq`
+-- Database: `rifq`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Appointment`
+-- Table structure for table `appointment`
 --
 
-CREATE TABLE `Appointment` (
-  `id` int NOT NULL,
-  `PatientID` int NOT NULL,
-  `DoctorID` int NOT NULL,
+CREATE TABLE `appointment` (
+  `id` int(11) NOT NULL,
+  `PatientID` int(11) NOT NULL,
+  `DoctorID` int(11) NOT NULL,
   `date` datetime NOT NULL,
   `time` time NOT NULL,
   `reason` text NOT NULL,
   `status` enum('Pending','Confirmed','Done') DEFAULT 'Pending'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `Appointment`
+-- Dumping data for table `appointment`
 --
 
-INSERT INTO `Appointment` (`id`, `PatientID`, `DoctorID`, `date`, `time`, `reason`, `status`) VALUES
+INSERT INTO `appointment` (`id`, `PatientID`, `DoctorID`, `date`, `time`, `reason`, `status`) VALUES
 (1, 1234, 1, '2025-02-14 00:00:00', '10:00:00', 'Routine check-up', 'Confirmed'),
 (2, 1234, 2, '2025-05-20 00:00:00', '15:00:00', 'Follow-up', 'Pending'),
-(3, 1235, 3, '2025-07-16 00:00:00', '17:00:00', 'Flu symptoms', 'Confirmed');
+(3, 1235, 3, '2025-07-16 00:00:00', '17:00:00', 'Flu symptoms', 'Done');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Doctor`
+-- Table structure for table `doctor`
 --
 
-CREATE TABLE `Doctor` (
-  `id` int NOT NULL,
+CREATE TABLE `doctor` (
+  `id` int(11) NOT NULL,
   `firstName` varchar(50) NOT NULL,
   `lastName` varchar(50) NOT NULL,
   `uniqueFileName` varchar(255) NOT NULL,
-  `SpecialityID` int NOT NULL,
+  `SpecialityID` int(11) NOT NULL,
   `emailAddress` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `Doctor`
+-- Dumping data for table `doctor`
 --
 
-INSERT INTO `Doctor` (`id`, `firstName`, `lastName`, `uniqueFileName`, `SpecialityID`, `emailAddress`, `password`) VALUES
-(1, 'Saleh', 'Abdullah', 'doctor1.png', 1, 'saleh@example.com', 'hashed_password4'),
-(2, 'Sara', 'Ahmad', 'doctor2.png', 2, 'sara@example.com', 'hashed_password5'),
-(3, 'Lulu', 'Mohamed', 'doctor3.png', 3, 'lulu@example.com', 'hashed_password6');
+INSERT INTO `doctor` (`id`, `firstName`, `lastName`, `uniqueFileName`, `SpecialityID`, `emailAddress`, `password`) VALUES
+(1, 'Saleh', 'Abdullah', 'doctor1.png', 1, 'saleh@example.com', '$2y$10$tPrZk4bpvSp8v4Gea8ML0OnkkI9BeXzIH7DKGhnF/BaPK7tz/XNpa'),
+(2, 'Sara', 'Ahmad', 'doctor2.png', 2, 'sara@example.com', '$2y$10$6eWvRp63ikytGEdnhq5W7uPsk7nTf3L6XN4Gpe6KKaXvoU07UYxje'),
+(3, 'Lulu', 'Mohamed', 'doctor3.png', 3, 'lulu@example.com', '$2y$10$2qFb07zK.LtHcNFpDCpefuEvOKPOp2FOmXJAPuZ.PvYzk9CnME1uK');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Medication`
+-- Table structure for table `medication`
 --
 
-CREATE TABLE `Medication` (
-  `id` int NOT NULL,
+CREATE TABLE `medication` (
+  `id` int(11) NOT NULL,
   `MedicationName` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `Medication`
+-- Dumping data for table `medication`
 --
 
-INSERT INTO `Medication` (`id`, `MedicationName`) VALUES
+INSERT INTO `medication` (`id`, `MedicationName`) VALUES
 (1, 'Amoxicillin Clavulanic Acid'),
 (2, 'Carprofen'),
 (3, 'Furosemide'),
@@ -95,45 +95,45 @@ INSERT INTO `Medication` (`id`, `MedicationName`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Patient`
+-- Table structure for table `patient`
 --
 
-CREATE TABLE `Patient` (
-  `id` int NOT NULL,
+CREATE TABLE `patient` (
+  `id` int(11) NOT NULL,
   `firstName` varchar(50) NOT NULL,
   `lastName` varchar(50) NOT NULL,
   `Gender` enum('Male','Female') NOT NULL,
   `DoB` date NOT NULL,
   `emailAddress` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `Patient`
+-- Dumping data for table `patient`
 --
 
-INSERT INTO `Patient` (`id`, `firstName`, `lastName`, `Gender`, `DoB`, `emailAddress`, `password`) VALUES
-(1234, 'Blue', 'Barron', 'Male', '2014-01-01', 'email@example.com', 'hashed_password1'),
-(1235, 'Meme', 'Smith', 'Female', '2024-02-23', 'meme@example.com', 'hashed_password2'),
-(1236, 'Koko', 'Ali', 'Male', '2020-07-16', 'koko@example.com', 'hashed_password3');
+INSERT INTO `patient` (`id`, `firstName`, `lastName`, `Gender`, `DoB`, `emailAddress`, `password`) VALUES
+(1234, 'Blue', 'Barron', 'Male', '2014-01-01', 'email@example.com', '$2y$10$y6hsAiMbFtLUUtPzKPv6QO.Zhn6rPlVq34kg5Xf6WdMkz0fxZ1t7C'),
+(1235, 'Meme', 'Smith', 'Female', '2024-02-23', 'meme@example.com', '$2y$10$3e7UZR/5dNzgo7.W0HCT6Od4FRdu1f8XNsk8fokT5Oc.AAx1N38OO'),
+(1236, 'Koko', 'Ali', 'Male', '2020-07-16', 'koko@example.com', '$2y$10$q0k0s9Et5Y3TYuOE5ezZeuND8BkrCG2Eoqt14AhUhqLaZguQeIbPe');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Prescription`
+-- Table structure for table `prescription`
 --
 
-CREATE TABLE `Prescription` (
-  `id` int NOT NULL,
-  `AppointmentID` int NOT NULL,
-  `MedicationID` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `prescription` (
+  `id` int(11) NOT NULL,
+  `AppointmentID` int(11) NOT NULL,
+  `MedicationID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `Prescription`
+-- Dumping data for table `prescription`
 --
 
-INSERT INTO `Prescription` (`id`, `AppointmentID`, `MedicationID`) VALUES
+INSERT INTO `prescription` (`id`, `AppointmentID`, `MedicationID`) VALUES
 (1, 1, 1),
 (2, 2, 2),
 (3, 3, 3);
@@ -141,19 +141,19 @@ INSERT INTO `Prescription` (`id`, `AppointmentID`, `MedicationID`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Speciality`
+-- Table structure for table `speciality`
 --
 
-CREATE TABLE `Speciality` (
-  `id` int NOT NULL,
+CREATE TABLE `speciality` (
+  `id` int(11) NOT NULL,
   `speciality` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `Speciality`
+-- Dumping data for table `speciality`
 --
 
-INSERT INTO `Speciality` (`id`, `speciality`) VALUES
+INSERT INTO `speciality` (`id`, `speciality`) VALUES
 (1, 'General Medicine'),
 (2, 'Pediatrics'),
 (3, 'Cardiology'),
@@ -164,46 +164,46 @@ INSERT INTO `Speciality` (`id`, `speciality`) VALUES
 --
 
 --
--- Indexes for table `Appointment`
+-- Indexes for table `appointment`
 --
-ALTER TABLE `Appointment`
+ALTER TABLE `appointment`
   ADD PRIMARY KEY (`id`),
   ADD KEY `PatientID` (`PatientID`),
   ADD KEY `DoctorID` (`DoctorID`);
 
 --
--- Indexes for table `Doctor`
+-- Indexes for table `doctor`
 --
-ALTER TABLE `Doctor`
+ALTER TABLE `doctor`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `emailAddress` (`emailAddress`),
   ADD KEY `SpecialityID` (`SpecialityID`);
 
 --
--- Indexes for table `Medication`
+-- Indexes for table `medication`
 --
-ALTER TABLE `Medication`
+ALTER TABLE `medication`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `Patient`
+-- Indexes for table `patient`
 --
-ALTER TABLE `Patient`
+ALTER TABLE `patient`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `emailAddress` (`emailAddress`);
 
 --
--- Indexes for table `Prescription`
+-- Indexes for table `prescription`
 --
-ALTER TABLE `Prescription`
+ALTER TABLE `prescription`
   ADD PRIMARY KEY (`id`),
   ADD KEY `AppointmentID` (`AppointmentID`),
   ADD KEY `MedicationID` (`MedicationID`);
 
 --
--- Indexes for table `Speciality`
+-- Indexes for table `speciality`
 --
-ALTER TABLE `Speciality`
+ALTER TABLE `speciality`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -211,64 +211,52 @@ ALTER TABLE `Speciality`
 --
 
 --
--- AUTO_INCREMENT for table `Appointment`
+-- AUTO_INCREMENT for table `appointment`
 --
-ALTER TABLE `Appointment`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `Doctor`
+-- AUTO_INCREMENT for table `medication`
 --
-ALTER TABLE `Doctor`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `medication`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `Medication`
+-- AUTO_INCREMENT for table `prescription`
 --
-ALTER TABLE `Medication`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `prescription`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `Patient`
+-- AUTO_INCREMENT for table `speciality`
 --
-ALTER TABLE `Patient`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1237;
-
---
--- AUTO_INCREMENT for table `Prescription`
---
-ALTER TABLE `Prescription`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `Speciality`
---
-ALTER TABLE `Speciality`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `speciality`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `Appointment`
+-- Constraints for table `appointment`
 --
-ALTER TABLE `Appointment`
-  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`PatientID`) REFERENCES `Patient` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`DoctorID`) REFERENCES `Doctor` (`id`) ON DELETE CASCADE;
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`PatientID`) REFERENCES `patient` (`id`),
+  ADD CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`DoctorID`) REFERENCES `doctor` (`id`);
 
 --
--- Constraints for table `Doctor`
+-- Constraints for table `doctor`
 --
-ALTER TABLE `Doctor`
-  ADD CONSTRAINT `doctor_ibfk_1` FOREIGN KEY (`SpecialityID`) REFERENCES `Speciality` (`id`) ON DELETE CASCADE;
+ALTER TABLE `doctor`
+  ADD CONSTRAINT `doctor_ibfk_1` FOREIGN KEY (`SpecialityID`) REFERENCES `speciality` (`id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `Prescription`
+-- Constraints for table `prescription`
 --
-ALTER TABLE `Prescription`
-  ADD CONSTRAINT `prescription_ibfk_1` FOREIGN KEY (`AppointmentID`) REFERENCES `Appointment` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `prescription_ibfk_2` FOREIGN KEY (`MedicationID`) REFERENCES `Medication` (`id`) ON DELETE CASCADE;
+ALTER TABLE `prescription`
+  ADD CONSTRAINT `prescription_ibfk_1` FOREIGN KEY (`AppointmentID`) REFERENCES `appointment` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `prescription_ibfk_2` FOREIGN KEY (`MedicationID`) REFERENCES `medication` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
